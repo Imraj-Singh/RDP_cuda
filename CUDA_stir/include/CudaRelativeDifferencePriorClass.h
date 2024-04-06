25a93c2e79637c488da504ace3b71874273f2866
Imraj-Singh <72553490+Imraj-Singh@users.noreply.github.com>
2024-04-06 15:34:14 +0000

    working as a child class, lots of copying

--- CUDA_stir/include/CudaRelativeDifferencePriorClass.h
+++ CUDA_stir/include/CudaRelativeDifferencePriorClass.h
@@ -3,20 +3,28 @@
 
 #include "stir/recon_buildblock/RelativeDifferencePrior.h"
 #include "stir/DiscretisedDensity.h"
+#include <cuda_runtime.h>
 
 START_NAMESPACE_STIR
 
 template <typename elemT>
 class CudaRelativeDifferencePriorClass : public RelativeDifferencePrior<elemT> {
-public:
-    static const char* const registered_name;
+    public:
+        // Name which will be used when parsing a GeneralisedPrior object
+        static const char* const registered_name;
 
-    CudaRelativeDifferencePriorClass();
+        // Constructors
+        CudaRelativeDifferencePriorClass();
+        CudaRelativeDifferencePriorClass(const bool only_2D, float penalization_factor, float gamma, float epsilon);
 
-    CudaRelativeDifferencePriorClass(const bool only_2D, float penalization_factor, float gamma, float epsilon);
+        // Overridden methods
+        virtual Succeeded set_up(shared_ptr<DiscretisedDensity<3, elemT>> const& target_sptr) override;
+        double compute_value(const DiscretisedDensity<3, elemT>& current_image_estimate) override;
+        void compute_gradient(DiscretisedDensity<3, elemT>& prior_gradient, const DiscretisedDensity<3, elemT>& current_image_estimate) override;
+    protected:
+        int z_dim, y_dim, x_dim;
+        dim3 grid_dim, block_dim;
 
-    double compute_value(const DiscretisedDensity<3, elemT>& current_image_estimate) override;
-    void compute_gradient(DiscretisedDensity<3, elemT>& prior_gradient, const DiscretisedDensity<3, elemT>& current_image_estimate) override;
 };
 
 END_NAMESPACE_STIR
--- CUDA_stir/src/main_cuda_class.cxx
+++ CUDA_stir/src/main_cuda_class.cxx
@@ -34,14 +34,10 @@ int main()
     prior.compute_gradient(*gradient_sptr, *density_sptr);
     const double my_prior_value = prior.compute_value(*density_sptr);
 
-    std::string gradient_filename = "rdp_gradient_cpp";
+    std::string gradient_filename = "rdp_gradient_cuda";
     output_file_format_sptr->write_to_file(gradient_filename, *gradient_sptr);
 
     /////// Return the prior value
     std::cout << "The Prior Value = " << my_prior_value << "\n";
-    // print kappa, gamma, epsilon
-    std::cout << "kappa = " << kappa << "\n";
-    std::cout << "gamma = " << gamma << "\n";
-    std::cout << "epsilon = " << epsilon << "\n";
     return EXIT_SUCCESS;
 }
